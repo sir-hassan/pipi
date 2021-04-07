5b118619f63a37f37edb6e46d9531bdf08fee893
Yassir Hassan <yassir.hassan@hetzner.com>
2021-04-07 21:38:19 +0200

    Add docs.

--- parse/parse.go
+++ parse/parse.go
@@ -2,8 +2,9 @@ package parse
 
 import "io"
 
-// Parser is the interface that parses input data to something other structure.
-// The intention is to isolate all the parsing logic details behind this simple interface.
+// Parser is the interface that parses input data to some other structure.
+// The intention is to isolate all the parsing logic details behind this simple
+// interface. Parser should be safe for concurrent use by multiple goroutines.
 type Parser interface {
 	Parse(input io.Reader) (interface{}, error)
 }
